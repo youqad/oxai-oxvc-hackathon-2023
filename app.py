631a94cc4fd7536b3f9a5e0b00874f26827a1975
Younesse Kaddar <younesse.kaddar@chch.ox.ac.uk>
2023-05-14 15:09:29 +0100

    new version

--- app.py
+++ app.py
@@ -1,94 +1,121 @@
 import os
-import pinecone
 import streamlit as st
-from langchain.document_loaders import TextLoader 
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader  
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings 
-from langchain.vectorstores import Chroma, Pinecone
-from langchain.chains import ConversationalRetrievalChain, LLMChain, SimpleSequentialChain
+from langchain.embeddings import OpenAIEmbeddings   
+from langchain.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain, LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
-from langchain.schema import (AIMessage, HumanMessage, SystemMessage)
 from langchain.chat_models import ChatOpenAI, ChatAnthropic
 from langchain import PromptTemplate
 from dotenv import load_dotenv, find_dotenv
+from langchain import PromptTemplate
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from io import StringIO
+from langchain.vectorstores import FAISS
+import PyPDF2
 
+# Load environment variables
+load_dotenv(find_dotenv())  
 
-# Create a function to get the feedback from the AI model
-def get_feedback(statement, format="pdf"):
-    # Get the predictions from the AI model
-    predictions = model.predict(statement)
+# Set page config  
+st.set_page_config(page_title="AI Statement Reviewer", page_icon="📚")  
 
-    # Create a list of feedback
-    feedback = []
-    for prediction in predictions:
-        feedback.append(prediction["feedback"])
+@st.cache_data
+def load_file(files):
+    st.info("`Analysing...`")
+    text = ""
+    for file_path in files:
+        file_extension = os.path.splitext(file_path.name)[1]
+        if file_extension == ".pdf":
+            pdf_reader = PyPDF2.PdfReader(file_path)
+            text += "".join([page.extractText() for page in pdf_reader.pages])
+        elif file_extension == ".txt":
+            stringio = StringIO(file_path.getvalue().decode("utf-8"))
+            text += stringio.read()
+        else:
+            st.warning('Please provide a text or pdf file.', icon="⚠️")
+    return text
 
+# Initialize session state
+if 'text' not in st.session_state:
+    st.session_state['text'] = ''
+
+# Create models  
+claude = ChatAnthropic()
+
+# Create retrieval chain
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
+
+template="You are an expert problem statement reviewer with an expertise in getting A-levels students studying {subject} admitted to their dream university: {university}."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template="Can you give constructive criticism to improve my problem statement: {statement}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+chain = LLMChain(llm=llm, prompt=chat_prompt)
+
+def get_feedback(text):
+    # Use a loading screen 
+    with st.spinner('🔄 Generating feedback...'): 
+        feedback = chain.predict(subject="English", university="Oxford University", statement=text, verbose=True)
+    print(feedback)
     return feedback
 
-# Create a function to display the feedback
 def display_feedback(feedback):
-    st.header("📝 Here's your feedback:")
-    st.write(feedback)
+    st.write("🌟 Here is the AI feedback:")
+    # Style the feedback
+    st.markdown(f'<p style="font-size: 20px">{feedback}</p>', unsafe_allow_html=True)
 
-# Create a main function
+
 def main():
-    """
-    AI Statement Reviewer App
-
-    This application uses OpenAI's GPT-4 and Anthropic's Claude language models, along with the LangChain framework, to provide an AI-driven statement review service for students applying to universities. 
-
-    The app reviews student personal statements and gives feedback on several aspects:
-    1. Grammar and Structure: The app evaluates the grammar and structural integrity of the personal statement, providing suggestions for improvement where necessary.
-    2. Tips and Recommendations: The app gives personalized tips and recommendations, encouraging students to engage in their own research and further study.
-
-    The application is designed to democratize access to high-quality personal statement advice, providing feedback that was previously only available to a select few. The ultimate goal is to enhance social mobility and level the playing field for all students, regardless of their background.
-
-    This application is a part of Afinity.io's suite of student advice services, which aim to provide comprehensive guidance to students choosing courses at the university level. It is also an important step towards Afinity.io's vision for 2030: to have every student make informed study choices through the Afinity platform.
-    """
-
-    load_dotenv(find_dotenv())
-
-    st.set_page_config(
-        page_title="AI Statement Reviewer",
-        page_icon="📚",
-        layout="centered",
-        initial_sidebar_state="expanded",
-    )
-
-    st.title('🎓 AI Statement Reviewer 📝')
-
-    st.header('By Afinity.io')
-
+    # Set page title and icon
+    st.title("🎓 AI Statement Reviewer 📝")
+    
+    # Add description 
+    st.header('✨ By Affinity.io ✨')
     st.markdown("""
-    This application uses AI to review and provide feedback on your university personal statement! 
+        This application uses advanced AI to review and provide feedback on your university personal statement! 👨‍🎓👩‍🎓
+        
+        Here's what it does:
 
-    Here's what it does:
-
-    1. 🧐 **Review your grammar and structure**: The AI, powered by GPT-4, will check your statement for any grammatical or structural issues.
-    2. 💡 **Provide tips and recommendations**: Claude, another advanced AI, gives personalized tips and recommendations to make your statement even better.
-
-    This tool is part of Afinity.io's mission to democratize access to high-quality advice for students, no matter their background.
-
-    Just upload your personal statement below, and let our AI give you feedback!
-    """)
-
-    uploaded_file = st.file_uploader("Upload your personal statement here", type=["txt", "docx", "pdf"])
-    text_input = st.text_area("Or paste your personal statement here:")
-
-    if uploaded_file is not None:
-        statement = uploaded_file.read().decode()
-        file_type = uploaded_file.type.split('/')[1]
-        if st.button('Get feedback for uploaded file'):
-            feedback = get_feedback(statement, file_type)
+        1. 🧐 **Review your grammar and structure**: Our AI, powered by OpenAI's Davinci and Anthropic's Claude, will check your statement
+        for grammar and structure, helping you present your ideas clearly and effectively.
+        2. 💡 **Provide useful tips and recommendations**: The AI will provide insightful tips to strengthen your statement.
+        3. 🌐 **Support for all students**: Our goal is to provide high-quality, personalized feedback to students from all backgrounds.  
+        
+        Just upload your statement or paste it in the box below, and let's get started! 🚀
+     """)
+    
+    # Get file or text input 
+    uploaded_file = st.file_uploader("📂 Upload your personal statement here", type=["pdf","docx","txt"], accept_multiple_files=True) 
+    text_input = st.text_area("💬 Or enter your personal statement here:", value=st.session_state['text'])
+    st.session_state['text'] = text_input
+    
+    # Get and display feedback
+    if uploaded_file is not None: 
+        # Load text from file
+        text = load_file(uploaded_file)  
+        if st.button("🔍 Get Feedback"):
+            feedback = get_feedback(text) 
             display_feedback(feedback)
     elif text_input:
-        if st.button('Get feedback for pasted text'):
-            feedback = get_feedback(text_input, "text")
+        if st.button("🔍 Get Feedback"):
+            feedback = get_feedback(text_input) 
             display_feedback(feedback)
     else:
-        st.write("📤 Please upload your personal statement or paste it into the text box.")
-    
-
-if __name__ == "__main__":
-    main() 
+        st.write("Please upload a file or enter your personal statement to get feedback. 📝") 
+if __name__ == "__main__": 
+    main()
