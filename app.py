396376808bd3cc1addfab6e4a6d982ed44d4ac66
Younesse Kaddar <younesse.kaddar@chch.ox.ac.uk>
2023-05-14 16:02:47 +0100

    added GPT-4

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader, Docx2txtLoader  
+# from langchain.document_loaders import PyPDFLoader, Docx2txtLoader  
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings   
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain, LLMChain
 from langchain.memory import ConversationBufferMemory
-from langchain.llms import OpenAI
+from langchain.vectorstores import FAISS
+
+# from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI, ChatAnthropic
-from langchain import PromptTemplate
 from dotenv import load_dotenv, find_dotenv
-from langchain import PromptTemplate
 from langchain.prompts import (
     ChatPromptTemplate,
     PromptTemplate,
@@ -24,19 +24,44 @@ from langchain.schema import (
     SystemMessage
 )
 from io import StringIO
-from langchain.vectorstores import FAISS
 import PyPDF2
 import docx
 
 # Load environment variables
-load_dotenv(find_dotenv())  
+load_dotenv(find_dotenv()) 
+
+st.set_page_config(page_title="AI Statement Reviewer", page_icon="📚")  
+
+# Models for selection 
+models = ['GPT-3.5', 'GPT-4', 'Claude']
+
+# Create models  
+chatgpt = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
+gpt4 = ChatOpenAI(model_name="gpt-4", temperature=0.3)
+claude = ChatAnthropic()
+
+# Initialize session state
+if 'model' not in st.session_state:
+    st.session_state['model'] = 'GPT-3.5-turbo'
+
+# Get model based on user selection
+selected_model = st.session_state['model']
+if selected_model == 'GPT-3.5':
+    llm = chatgpt 
+elif selected_model == 'GPT-4':
+    llm = gpt4
+elif selected_model == 'Claude':
+    llm = claude
+
+# Add model selection to sidebar
+st.sidebar.header('Select Model') 
+selected_model = st.sidebar.selectbox('Choose AI model', models)
+st.session_state['model'] = selected_model
+
 
 # Universities and Majors for selection
-universities = ['Oxford University', 'St Andrews University', 'Warwick University', 'University of Sheffield', 'University of Cambridge', 'Infer from statement']
-majors = ['English', 'Computer Science', 'Engineering', 'Mathematics', 'Biology', 'Infer from statement']
-
-# Set page config  
-st.set_page_config(page_title="AI Statement Reviewer", page_icon="📚")  
+universities = ["Infer from statement", "University of Aberdeen", "Abertay University", "Aberystwyth University", "Anglia Ruskin University", "Arden University", "Arts University Bournemouth", "Aston University", "Bangor University", "Bath Spa University", "University of Bath", "University of Bedfordshire", "Birkbeck, University of London", "Birmingham City University", "University of Birmingham", "Bishop Grosseteste University", "University of Bolton", "Bournemouth University", "University of Brighton", "University of Bristol", "British Academy of Jewellery", "British College of Osteopathic Medicine", "Brunel University London", "University of Buckingham", "Buckinghamshire New University", "University of Cambridge", "Canterbury Christ Church University", "Cardiff Metropolitan University", "Cardiff University", "University of Chester", "University of Chichester", "City, University of London", "Coventry University", "Cranfield University", "University for the Creative Arts", "University of Cumbria", "De Montfort University", "University of Derby", "University of Dundee", "Durham University", "University of East Anglia", "University of East London", "Edge Hill University", "Edinburgh Napier University", "University of Edinburgh", "University of Essex", "University of Exeter", "Falmouth University", "Glasgow Caledonian University", "Glasgow School of Art", "University of Glasgow", "University of Gloucestershire", "Goldsmiths, University of London", "University of Greenwich", "Harper Adams University", "Heriot-Watt University", "University of Hertfordshire", "University of the Highlands and Islands", "University of Huddersfield", "University of Hull", "Imperial College London", "Keele University", "University of Kent", "King's College London, University of London", "Kingston University", "Lancaster University", "Leeds Arts University", "Leeds Beckett University", "Leeds Trinity University", "University of Leeds", "University of Leicester", "University of Lincoln", "Liverpool Hope University", "Liverpool Institute for Performing Arts", "Liverpool John Moores University", "University of Liverpool", "London Metropolitan University", "London School of Economics and Political Science, University of London", "London School of Hygiene and Tropical Medicine, University of London", "London South Bank University", "Loughborough University", "Manchester Metropolitan University", "University of Manchester", "Middlesex University", "Newcastle University", "Newman University", "University of Northampton", "Northeastern University", "University of Northumbria at Newcastle", "Norwich University of the Arts", "Nottingham Trent University", "University of Nottingham", "The Open University", "Oxford Brookes University", "University of Oxford", "University of Plymouth", "University of Portsmouth", "Queen Margaret University, Edinburgh", "Queen Mary University of London", "Queen's University Belfast", "Ravensbourne University London", "University of Reading", "Regent's University London", "Robert Gordon University", "University of Roehampton", "Rose Bruford College", "Royal Agricultural University", "Royal Central School of Speech and Drama", "Royal College of Art", "Royal College of Music", "Royal Conservatoire of Scotland", "Royal Holloway, University of London", "Royal Veterinary College, University of London", "University of Salford", "University of Sheffield", "Sheffield Hallam University", "University of South Wales", "University of Southampton", "University of St Andrews", "University of London St George's", "University of St Mark and St John", "University of Stirling", "University of Strathclyde", "University of Suffolk", "University of Sunderland", "University of Surrey", "University of Sussex", "Swansea University", "Teesside University", "Trinity Laban Conservatoire of Music and Dance", "Trinity Saint David, University of Wales", "Truro and Penwith College", "St Mary's University, Twickenham", "UCL (University College London)", "Ulster University", "University Campus Oldham", "University of Warwick", "University of Warwickshire", "University of the West of England, Bristol", "University of the West of Scotland", "University of Westminster", "University of Winchester", "Wirral Metropolitan College", "University of Wolverhampton", "University of Worcester", "Writtle University College", "York College", "York St John University", "University of York"]
+majors = ["Infer from statement", "Accounting and Finance", "Aeronautical and Manufacturing Engineering", "Agriculture and Forestry", "Anatomy and Physiology", "Anthropology", "Archaeology", "Architecture", "Art and Design", "Biological Sciences", "Building", "Business and Management Studies", "Chemical Engineering", "Chemistry", "Civil Engineering", "Classics and Ancient History", "Communication and Media Studies", "Complementary Medicine", "Computer Science", "Counselling", "Creative Writing", "Criminology", "Dentistry", "Drama Dance and Cinematics", "Economics", "Education", "Electrical and Electronic Engineering", "English", "Fashion", "Film Making", "Food Science", "Forensic Science", "General Engineering", "Geography and Environmental Sciences", "Geology", "Health And Social Care", "History", "History of Art Architecture and Design", "Hospitality Leisure Recreation and Tourism", "Information Technology", "Land and Property Management", "Law", "Linguistics", "Marketing", "Materials Technology", "Mathematics", "Mechanical Engineering", "Medical Technology", "Medicine", "Music", "Nursing", "Occupational Therapy", "Pharmacology and Pharmacy", "Philosophy", "Physics and Astronomy", "Physiotherapy", "Politics", "Psychology", "Robotics", "Social Policy", "Social Work", "Sociology", "Sports Science", "Veterinary Medicine", "Youth Work"]
 
 @st.cache_data
 def load_file(files):
@@ -60,12 +85,6 @@ def load_file(files):
 # Initialize session state
 if 'text' not in st.session_state:
     st.session_state['text'] = ''
-
-# Create models  
-claude = ChatAnthropic()
-
-# Create retrieval chain
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
 
 template="You are an expert problem statement reviewer with an expertise in getting A-levels students studying {subject} admitted to their dream university: {university}."
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
